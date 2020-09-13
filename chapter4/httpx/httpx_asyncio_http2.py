c762879cc624288984d74898ca6a407ec7fa36bf
jmortega <jmortegac@protonmail.com>
2020-09-13 22:43:01 +0200

    Mastering-Python-for-Networking-and-Security-Second-Edition

--- chapter4/httpx/httpx_asyncio_http2.py
+++ chapter4/httpx/httpx_asyncio_http2.py
@@ -3,7 +3,7 @@ import asyncio
 
 async def resquest_http2():
 	async with httpx.AsyncClient(http2=True) as client:
-		response = await client.get("http://www.google.es")
+		response = await client.get("https://www.google.es")
 		print(response)
 		print(response.http_version)
 		
